a29be16122cb547064dcf63b04b398c4273e841e
Rmarieta <raphael.marietan@epfl.ch>
2023-01-08 18:28:10 +0100

    changes

---
+++ app.py
@@ -0,0 +1,24 @@
+import pickle
+from flask import Flask, request, app, jsonify, url_for, render_template
+import numpy as np
+import pandas as pd
+
+app = Flask(__name__)
+
+model = pickle.load(open('src/model.pkl','rb'))
+std_scaler = pickle.load(open('src/scaling.pkl','rb'))
+
+@app.route('/')
+def home() :
+    return render_template('home_page.html')
+
+@app.route('/predictapi',methods=['POST'])
+def predictapi() :
+    data = request.json['data']
+    new_data = std_scaler.transform(np.array(list(data.values())).reshape(1,-1))
+    y = model.predict(new_data)
+    return jsonify(y[0])
+
+if __name__ == "__main__" :
+
+    app.run(debug=True)
