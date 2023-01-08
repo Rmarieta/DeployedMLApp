719f7ff1ffc751db701a13b428281275b9f7f913
Rmarieta <raphael.marietan@epfl.ch>
2023-01-08 18:38:32 +0100

    changes

--- app.py
+++ app.py
@@ -19,6 +19,13 @@ def predictapi() :
     y = model.predict(new_data)
     return jsonify(y[0])
 
+@app.route('/predict',methods=['POST'])
+def predict() :
+    data = [float(elem) for elem in request.form.values()]
+    inp = std_scaler.transform(np.array(data).reshape(1,-1))
+    out = model.predict(inp)[0]
+    return render_template("home_page.html",prediction=f"Predicted housing price : {out}")
+
 if __name__ == "__main__" :
 
     app.run(debug=True)
